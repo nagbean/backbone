bfa876d06060af89b5fe856579453833f56df92f
Cho Kim <4cho.kim@gmail.com>
2014-01-27 02:39:19 -0500

    basic functionality

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
 $(document).ready(function() {
-  console.log("document is ready, cho");
+
+  var TodoItem = Backbone.Model.extend({
+    defaults: {
+    	description: '...',
+    	status: 'uncompleted'
+    }
+  });
+
+  var TodoView = Backbone.View.extend({
+  	model: TodoItem,
+
+    tagName: 'li', 
+    className: 'status',
+
+    template: _.template('<%= description %>'),
+
+    render: function() {
+      var attributes = this.model.toJSON();
+      this.$el.html(this.template(attributes));
+    } 
+  });
+
+  var todoItem = new TodoItem();
+  var todoView = new TodoView({model: todoItem});
+
+  todoView.render();
+
+  $('ul').html(todoView.el);
+
+  console.log(todoView.model.get('status'));
 });
